dc771c4c7e4675cf9176f414675f9c177b808bfa
deepak <devwebwithdeepak@gmail.com>
2024-03-10 22:14:03 +0530

    hamburger icon

--- src/Components/body/Information.jsx
+++ src/Components/body/Information.jsx
@@ -3,20 +3,20 @@ import { Link } from "react-router-dom";
 
 function Information() {
   return (
-    <main className="min-h-screen flex flex-col lg:flex-row justify-between items-center lg:items-start">
-      <div className="details w-full px-2 lg:w-1/2 py-8 lg:py-32  lg:px-10 gap-3 text-center lg:text-left ">
-        <h1 className="text-2xl lg:text-3xl font-bold">Quality Medical Care Delivered</h1>
-        <h1 className="text-2xl lg:text-3xl py-2 font-bold">At The <span className="text-2xl lg:text-3xl py-2 font-bold text-orange-400">Comfort of Your Home</span></h1>
-        <p className="text-base lg:text-lg p-4 lg:p-6 font-thin">
+    <main className="min-h-screen flex flex-col lg:flex-row justify-center lg:justify-between items-center">
+      <div className="details w-full lg:w-1/2 mr-40 px-4 lg:px-10 py-8 lg:py-16 gap-3 text-center lg:text-center">
+        <h1 className="text-2xl lg:text-4xl font-bold">Quality Medical Care Delivered</h1>
+        <h1 className="text-2xl lg:text-4xl py-2 font-bold">At The <span className="text-2xl lg:text-5xl py-2 font-bold text-orange-400 unde">Comfort of Your Home</span></h1>
+        <p className="text-base lg:text-lg p-4 lg:p-6 font-normal">
           Aadesh Health Care delivers quality care with compassion. We bring quality medical care into our patients’ homes and aim to make primary healthcare not only more accessible but also more affordable and accountable to our patient’s needs.
         </p>
-        <button className="p-2 mt-4 lg:mt-10 bg-green-600 rounded-lg font-bold text-white">
+        <button className="p-4 mt-4 lg:mt-10 bg-green-600 rounded-md font-bold text-white">
           <Link to="/book">BOOK NOW</Link>
         </button>
       </div>
-      <div className="imglogo p-20 m-10 w-full lg:w-[40vw] flex justify-center lg:justify-end mt-10 lg:mt-0 order-first lg:order-none">
+      <div className="imglogo p-10 lg:p-20 w-full lg:w-[40vw] flex justify-center order-first lg:order-none">
         <img
-          className="h-[20rem] lg:h-[30rem] w-[20rem] lg:w-[30rem] rounded-full border-green-300 border-8"
+          className="h-[20rem] lg:h-[30rem] w-[20rem] lg:w-[30rem] rounded-full border-green-300 border-8 mt-12"
           src="https://medicrov.com/wp-content/uploads/2024/01/Untitled-design.png"
           alt=""
         />
