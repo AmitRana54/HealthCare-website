47843c57693ab33672ce3d0dd231a01e9831e094
AmitRana54 <109697933+AmitRana54@users.noreply.github.com>
2024-03-14 20:12:21 +0530

    changesFiles

--- src/Components/body/Information.jsx
+++ src/Components/body/Information.jsx
@@ -27,7 +27,7 @@ function Information() {
         </div>
         <div className="imglogo h-20rem w-20rem lg:h-[40vw] mb-1 lg:mb-12 p-3 lg:p-8 w-full lg:w-[40vw] flex justify-center order-first lg:order-none">
           <img
-            className="h-[25rem] lg:h-[25rem] w-[25rem] lg:w-[25rem] rounded-full border-[#0D857B] border-8 mt-16 lg:mt-0"
+            className="h-[25rem] lg:h-[25rem] w-[25rem] lg:w-[25rem] rounded-full border-[#0D857B] border-8 mt-9gi lg:mt-0"
             src="https://medicrov.com/wp-content/uploads/2024/01/Untitled-design.png"
             alt=""
           />
